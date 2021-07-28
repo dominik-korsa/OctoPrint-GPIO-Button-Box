16ade9d1af22d54e5589ffd118eaf72940d36f66
Dominik Korsa <dominik.korsa@gmail.com>
2021-07-28 22:38:47 +0200

    Call strip.show in draw_led

--- octoprint_gpio_button_box/led.py
+++ octoprint_gpio_button_box/led.py
@@ -38,3 +38,4 @@ class LedManager:
     def draw_led(self):
         self.strip.setPixelColor(0, Color(0, 255, 0) if self.psu_on else Color(255, 0, 0))
         self.strip.setPixelColor(1, Color(0, 0, 255))
+        self.strip.show()
