aee7cd85867457c72537a562d071e436fad36e70
Dominik Korsa <dominik.korsa@gmail.com>
2021-07-28 21:12:28 +0200

    Comment out led manager usage

--- octoprint_gpio_button_box/plugin.py
+++ octoprint_gpio_button_box/plugin.py
@@ -2,7 +2,7 @@ import octoprint.plugin
 import octoprint.printer
 
 from .button_handler import ButtonHandler
-from .led import LedManager
+# from .led import LedManager
 from .printer_state import PrinterState
 
 
@@ -14,7 +14,7 @@ class GPIOButtonBox(
         self.start_button = None
         self.pause_button = None
         self.power_button = None
-        self.led_manager = None
+        # self.led_manager = None
         self.psucontrol_helpers = None
 
     def on_plugin_enabled(self):
@@ -22,8 +22,8 @@ class GPIOButtonBox(
         self.start_button = ButtonHandler(2, on_short_click=self.on_resume_click)
         self.pause_button = ButtonHandler(3, on_short_click=self.on_pause_click, on_long_click=self.on_cancel_click)
         self.power_button = ButtonHandler(4, on_short_click=self.on_power_toggle, on_long_click=self.on_power_stop)
-        self.led_manager = LedManager(self)
-        self.led_manager.start()
+        # self.led_manager = LedManager(self)
+        # self.led_manager.start()
 
     def on_plugin_disabled(self):
         self.start_button.close()
@@ -35,9 +35,9 @@ class GPIOButtonBox(
                 self._logger.info("PSU enabled")
             else:
                 self._logger.info("PSU disabled")
-        elif event == "PrinterStateChanged":
-            if self.led_manager is not None:
-                self.led_manager.update_printer_state()
+        # elif event == "PrinterStateChanged":
+            # if self.led_manager is not None:
+            #     self.led_manager.update_printer_state()
 
     def on_resume_click(self):
         self._printer.resume_print()
